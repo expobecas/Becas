97e6bc0bc9bd91aa3b6ad49457c1d92d23d033a3
IMPERVENUS\XAVI <fernanxavi58@gmail.com>
2018-05-19 21:33:50 -0600

    actulizacion

--- public/solicitud/solicitud.php
+++ public/solicitud/solicitud.php
@@ -1,6 +1,6 @@
 <?php
 require_once("../../app/views/public/templates/page.class.php");
 Page::templateHeader("");
-require_once("../../app/controllers/public/solicitud_controller.php");
+require_once("../../app/controllers/public/solicitud/solicitud_controller.php");
 Page::templateFooter();
 ?>

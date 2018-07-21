59ca5c7682efe8f9526047a630ce3d938d16d4ee
expobecas <37518924+expobecas@users.noreply.github.com>
2018-07-20 18:58:53 -0600

    Indentar

--- app/controllers/dashboard/ingresar/acceder_controller.php
+++ app/controllers/dashboard/ingresar/acceder_controller.php
@@ -1,45 +1,46 @@
-<?php 
-require_once("../../app/models/usuario.class.php");
-try{
+<?php
+require_once("../../app/models/usuario.class.php"); //Llamando al modelo
+try {
     $usuarios = new Usuario;
-    if($usuarios->getUsuarios()){
-        if(isset($_POST['ingresar'])){
+    if ($usuarios->getUsuarios()) {
+        if (isset($_POST['ingresar'])) {
             $_POST = $usuarios->validateForm($_POST);
-            if($usuarios->setUsuario($_POST['usuario'])){
-                if($usuarios->checkUsuario()){
-                    if($usuarios->setClave($_POST['clave'])){
-                        if($usuarios->checkClave()){
+            if ($usuarios->setUsuario($_POST['usuario'])) {
+                if ($usuarios->checkUsuario()) {
+                    if ($usuarios->setClave($_POST['clave'])) {
+                        if ($usuarios->checkClave()) {
                             $_SESSION['id_usuario'] = $usuarios->getId();
-                            $_SESSION['usuario'] = $usuarios-> getUsuario();
-                            $_SESSION['id_tipo'] = $usuarios-> getTipo();
-                            echo $_SESSION['id_tipo'];
-                            if($_SESSION['id_tipo'] == 1){
+                            $_SESSION['usuario']    = $usuarios->getUsuario();
+                            $_SESSION['id_tipo']    = $usuarios->getTipo();
+                            echo $_SESSION['id_tipo'];//Tomando el tipo, para redirigirle según su cargo
+                            if ($_SESSION['id_tipo'] == 1) {
                                 Page::showMessage(1, "Autenticación correcta", "../../dashboard/index/index.php");
                             }
-                            if($_SESSION['id_tipo'] == 2){
-                                Page::showMessage(1, "Autenticación correcta", "../../dashboard/account/index.php");
+                            if ($_SESSION['id_tipo'] == 2) {
+                                Page::showMessage(1, "Autenticación correcta", "../../public/jefes/index/index.php");
                             }
-                            if($_SESSION['id_tipo'] == 3){
+                            if ($_SESSION['id_tipo'] == 3) {
                                 Page::showMessage(1, "Autenticación correcta", "../../dashboard/index/index.php");
                             }
-                        }else{
+                        } else {
                             throw new Exception("Clave incorrecta");
                         }
-                    }else{
+                    } else {
                         throw new Exception("Clave menor a 6 caracteres");
                     }
-                }else{
+                } else {
                     throw new Exception("Usuario invalido");
                 }
-            }else{
+            } else {
                 throw new Exception("Usuario incorrecto");
             }
         }
-    }else{
+    } else {
         Page::showMessage(3, "No hay usuarios disponibles");
     }
-}catch(Exception $error){
-    Page::showMessage(2,$error->getMessage(), null);
+}
+catch (Exception $error) {
+    Page::showMessage(2, $error->getMessage(), null);
 }
 require_once("../../app/views/dashboard/ingresar/acceder_view.php");
 ?> 

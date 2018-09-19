88c2dbcddf6ccb7783e08233f16730e1e6b7798c
XavierCanjura <36569554+XavierCanjura@users.noreply.github.com>
2018-09-18 18:26:28 -0600

    Add files via upload

--- app/views/dashboard/casos/create_view.php
+++ app/views/dashboard/casos/create_view.php
@@ -1,33 +1,29 @@
 <h3 class="center">Crear Caso</h3>
 <form method="POST"  autocomplete="off" id="caso" encrype="multipart/form-data">
     <div class="row">
-        <div class="input-field col l8 m8 offset-l3">
-            <textarea id="descripcion" class="materialize-textarea" name="descripcion" data-length="500"></textarea>
-            <label for="descripcion">Escribir la descripción del caso</label>
+        <div class="input-field col l8 m8 s12 offset-l3">
+            <textarea id="descripcion_c" class="materialize-textarea" name="descripcion_c" data-length="500"></textarea>
+            <label for="descripcion_c">Escribir la descripción del caso</label>
         </div>
-        <div class="file-field input-field col l8 offset-l3">
+        <div class="file-field input-field col l8 m8 s12 offset-l3">
             <div class="btn">
                 <span>imagenes</span>
-                <input type="file" id="imagenes" multiple>
+                <input type="file" name="imagenes[]" id="imagenes" multiple/>
             </div>
             <div class="file-path-wrapper">
-                <input class="file-path validate" type="text" placeholder="Seleccione una o varias imagenes">
+                <input class="file-path validate" type="text" placeholder="Seleccione una o varias imagenes"/>
             </div>
         </div>
-    </div>
-
-    <div class="row">
-        <div class="col offset-l8">
-            <button type="submit" name="rechazar" class="waves-effect waves-light red btn">Rechazar</button>
-            <button type="submit" name="aprobar" class="waves-effect waves-light green btn">Aprobar</button>
-        </div>
-
         <div class="col offset-l3">
-            <a id="subir" class="waves-effect waves-light red btn">Subir</a>
             <div id="vista"></div>
         </div>
         
     </div>
 
-    
+    <div class="row">
+        <div class="col offset-l8">
+            <a id="rechazar" class="waves-effect waves-light red btn">Rechazar</a>
+            <a id="aprobar" class="waves-effect waves-light green btn">Aprobar</a>
+        </div>
+    </div>
 </form>
